a51acbc2c13978d5d89e837cbe074d2b0e9a9439
notleysa <notley.sa@gmail.com>
2017-12-01 17:43:01 -0500

    Cleaned up some files for submission.

--- Scripts/correlation.py
+++ Scripts/correlation.py
@@ -10,7 +10,7 @@ import matplotlib.pylab as pylab
 
 import seaborn as sns
 
-#%matplotlib inline
+%matplotlib inline
 mpl.style.use( 'ggplot' )
 sns.set_style( 'white' )
 pylab.rcParams[ 'figure.figsize' ] = 8, 6
@@ -29,18 +29,32 @@ def plot_correlation_map( df ):
         annot_kws= {'fontsize': 12}
     )
 
-fname_weather = "NYC_Transportation_Weather_Study/Datasets/Wunderground/Monthly_Weather_Raw_JFK_Updated-Seaborn.csv"
-fname_stats = "NYC_Transportation_Weather_Study/Datasets/new_stats.csv"
+
+fname_weather = "Datasets/Wunderground/Monthly_Weather_Raw_JFK_Updated.csv"
+fname_stats = "Datasets/new_stats_green_tlc.csv"
 
 weather = pd.read_csv(fname_weather)
 stats = pd.read_csv(fname_stats)
 
 weather.drop('Month', axis=1, inplace=True)
 weather.drop('Day', axis=1, inplace=True)
+weather.drop('Sea Level Press. (in) - Max', axis=1, inplace=True)
+weather.drop('Sea Level Press. (in) - Min', axis=1, inplace=True)
+weather.drop('Temp. (Degrees F) - Max', axis=1, inplace=True)
+weather.drop('Temp. (Degrees F) - Min', axis=1, inplace=True)
+weather.drop('Humidity (%) - Max', axis=1, inplace=True)
+weather.drop('Humidity (%) - Min', axis=1, inplace=True)
+weather.drop('Visibility (mi) - Max', axis=1, inplace=True)
+weather.drop('Visibility (mi) - Min', axis=1, inplace=True)
+weather.drop('Wind (mph) - Max', axis=1, inplace=True)
+weather.drop('Wind (mph) - Max?!?', axis=1, inplace=True)
+weather.drop('Dew Point (Degrees F) - Max', axis=1, inplace=True)
+weather.drop('Dew Point (Degrees F) - Min', axis=1, inplace=True)
+
 
 frames = [stats, weather]
 result = pd.concat(frames, axis=1)
 result.drop('Event', axis=1, inplace=True)
-result.drop('Date', axis=1, inplace=True)
+#result.drop('Date', axis=1, inplace=True)
 
 plot_correlation_map(result)
